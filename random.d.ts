927e62ee20c41feca380937ede7f7d07eaaf2f3e
Tomm <tomm.huth@hotmail.com>
2022-10-17 20:44:12 +0200

    type fixes

--- random.d.ts
+++ random.d.ts
@@ -1,9 +1,11 @@
 interface Random {
-    id:() => string;
-    boolean:(likelihood: number) => boolean;
-    float:(min: number, max: number) => number;
-    integer: (min: number, max: number) => number;
-    pick: <T>(...items: T[]) => T;
+    id: () => string,
+    boolean: (likelihood: number) => boolean,
+    float: (min: number, max: number) => number,
+    integer: (min: number, max: number) => number,
+    pick: <T>(...items: T[]) => T
 }
 
-export default Random
+declare const random: Random
+
+export default random
